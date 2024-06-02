8b93855c3ab40e3a67fe567c22797f391f9426b1
Aman Kumar <cs21b001@iittp.ac.in>
2024-06-02 09:50:11 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -37,5 +37,12 @@ const PORT = process.env.PORT || 5000;
 
 
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => {
+        const server = app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+        process.on('SIGTERM', () => {
+            server.close(() => {
+                console.log('Process terminated')
+            })
+        })
+    })
     .catch((error) => console.log(error.message));
